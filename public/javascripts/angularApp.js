2c2ba32cee8001331ba9c6fa04f6a11c9f534996
brown6285 <curtis.brown@byu.edu>
2016-04-02 02:40:38 +0000

    first commit

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,14 @@
+angular.module('clusterApp', [])
+.controller('MainCtrl', [
+'$scope', '$http',
+function($scope, $http){
+  $scope.cluster = [];
+    for(var i =0; i<100; i++) {
+      $http.get('/pid').success(function(data){
+        console.log("getAll");
+        console.log(data);
+        $scope.cluster.push(data);
+      });
+  }
+} 
+]); 
